84ef769cdecbd8d76c8d5904d13ee935f0694898
zwippie <zwippie@gmail.com>
2017-12-19 23:28:32 +0100

    Remove infi puzzle

--- mix.exs
+++ mix.exs
@@ -23,7 +23,6 @@ defmodule AOC.Mixfile do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      {:mogrify, "~> 0.5.6"},
     ]
   end
 end
